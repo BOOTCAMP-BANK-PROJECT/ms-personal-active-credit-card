30afac6dd43fa26e0ca7dc1022d3c9f0300db2f2
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-20 14:06:26 -0500

    scaffolding

---
+++ src/main/java/com/bootcamp/personal/active/creditcard/CreditCardApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.active.creditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/personal/active/creditcard/CreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.active.creditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
